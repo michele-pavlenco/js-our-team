d6e3952b4b89a20ac83db13562700f07fc3891ba
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-07 17:21:23 +0200

    added-ciclo

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
-const cards = [{
-    foto : "img/wayne-barnett-founder-ceo.jpg",
-    nome : "Wayne Barrent",
-    ruolo : "Founder & CEO"
-},
-{
-    foto : "img/angela-caroll-chief-editor.jpg",
-    nome : "Angela Caroll",
-    ruolo : "Chief Editor"
-},
-{
-    foto : "img/walter-gordon-office-manager.jpg",
-    nome : "Walter Gordon",
-    ruolo : "Office Manager"
-},
-{
-    foto : "img/angela-lopez-social-media-manager.jpg",
-    nome : "Angela Lopez",
-    ruolo : "Social Media Manager"
-},
-{
-    foto : "img/scott-estrada-developer.jpg",
-    nome : "Scott Estrada",
-    ruolo : "Developer"
-},
-{
-    foto : "img/barbara-ramos-graphic-designer.jpg",
-    nome : "Barbara Ramos",
-    ruolo : "Graphic Designer"
-}
-]
+const cards = [
+    {
+        foto: "img/wayne-barnett-founder-ceo.jpg",
+        nome: "Wayne Barrent",
+        ruolo: "Founder & CEO"
+    },
+    {
+        foto: "img/angela-caroll-chief-editor.jpg",
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor"
+    },
+    {
+        foto: "img/walter-gordon-office-manager.jpg",
+        nome: "Walter Gordon",
+        ruolo: "Office Manager"
+    },
+    {
+        foto: "img/angela-lopez-social-media-manager.jpg",
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager"
+    },
+    {
+        foto: "img/scott-estrada-developer.jpg",
+        nome: "Scott Estrada",
+        ruolo: "Developer"
+    },
+    {
+        foto: "img/barbara-ramos-graphic-designer.jpg",
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer"
+    }
+];
 
-const CARDCONTAINER = document.getElementsByClassName("team-container")
+// const CARDCONTAINER = document.getElementsByClassName("team-container")
 
-const TEAMCARD = document.createElement("div")
-TEAMCARD.setAttribute("class", "team-card")
+// const TEAMCARD = document.createElement("div")
+// TEAMCARD.setAttribute("class", "team-card");
+// //TEAMCARD.append(CARDCONTAINER)
+// //CARDCONTAINER.textContent = TEAMCARD
+// //CARDCONTAINER.innerHTML = TEAMCARD
 
-const CARDIMAGE = document.createElement("div")
-CARDIMAGE.setAttribute("class", "card-image")
+// const CARDIMAGE = document.createElement("div")
+// CARDIMAGE.setAttribute("class", "card-image")
 
-const CARDTEXT = document.createElement("div")
-CARDTEXT.setAttribute("class", "card-text")
+// const CARDTEXT = document.createElement("div")
+// CARDTEXT.setAttribute("class", "card-text")
 
-console.log(CARDTEXT)
+// console.log(CARDCONTAINER, TEAMCARD)
+
+// for (let i = 0; i < cards.length; i++) {
+
+// }
+
+
+// let miaStringa  = 'ABC';
+
+
+// miaStringa += 'XYZ';
+
 
 
 // for(let key in cards[0]){
@@ -51,3 +66,22 @@ console.log(CARDTEXT)
 // for(let i = 0; i < cards.length; i++){
 //     console.log(cards[i].foto)
 // }
+const CARDCONTAINER = document.getElementsByClassName("team-container");
+
+for (let i = 0; i < cards.length; i++) {
+    CARDCONTAINER.innerHTML += `
+    <div class="team-card"> 
+          <div class="card-image"> 
+            <img src="${cards[i].foto}" />
+          </div>
+          <div class="card-text"> 
+          <h3>${cards.nome}</h3>
+          <p>Founder & CEO</p>
+        </div>
+      </div>
+        </div>`
+        console.log(CARDCONTAINER)
+    console.log(cards[i].foto);
+    console.log(cards[i].nome);
+}
+
