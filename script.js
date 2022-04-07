88e932b437fa59f88b6ce6bb41d6aca99731ff71
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-07 17:48:44 +0200

    end

--- script.js
+++ script.js
@@ -30,6 +30,25 @@ const cards = [
         ruolo: "Graphic Designer"
     }
 ];
+const CARDCONTAINER = document.querySelector(".team-container");
+
+for (let i = 0; i < cards.length; i++) {
+    CARDCONTAINER.innerHTML += `
+    <div class="team-card"> 
+
+        <div class="card-image"> 
+           <img src="${cards[i].foto}" />
+        </div>
+
+        <div class="card-text"> 
+           <h3>${cards[i].nome}</h3>
+           <p>${cards[i].ruolo}</p>
+        </div>
+
+    </div>
+       `
+}
+
 
 // const CARDCONTAINER = document.getElementsByClassName("team-container")
 
@@ -50,38 +69,4 @@ const cards = [
 // for (let i = 0; i < cards.length; i++) {
 
 // }
-
-
-// let miaStringa  = 'ABC';
-
-
-// miaStringa += 'XYZ';
-
-
-
-// for(let key in cards[0]){
-//     console.log(key)
-//     console.log(cards[0][key])
-// }
-// for(let i = 0; i < cards.length; i++){
-//     console.log(cards[i].foto)
-// }
-const CARDCONTAINER = document.getElementsByClassName("team-container");
-
-for (let i = 0; i < cards.length; i++) {
-    CARDCONTAINER.innerHTML += `
-    <div class="team-card"> 
-          <div class="card-image"> 
-            <img src="${cards[i].foto}" />
-          </div>
-          <div class="card-text"> 
-          <h3>${cards.nome}</h3>
-          <p>Founder & CEO</p>
-        </div>
-      </div>
-        </div>`
-        console.log(CARDCONTAINER)
-    console.log(cards[i].foto);
-    console.log(cards[i].nome);
-}
 
